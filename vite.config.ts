45c5a98e96f23b9dcabbdd4384c4e845c91f89fa
karan gerrard <karan.gerrard@gmail.com>
2026-04-25 22:46:53 +0530

    Added configuration for dev server for mobile debugging

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,9 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
-  server: { port: 3000 },
+  server:{
+    host: '0.0.0.0',
+    port: 3000,
+  },
   build: { outDir: 'dist' }
 })
